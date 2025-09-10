6c12c90d6fe8b2d3a318f2d6f57c4351a538c345
Sergij <sl.zhurba1@gmail.com>
2025-09-10 15:56:54 +0300

    Get Product Details and Show - finished

--- controllers/home/homeController.js
+++ controllers/home/homeController.js
@@ -2,6 +2,11 @@ const productModel = require("../../models/productModel");
 const categoryModel = require("../../models/categoryModel");
 const { responseReturn } = require("../../utiles/response");
 const queryProducts = require("../../utiles/queryProducts");
+const reviewModel = require("../../models/reviewModel");
+const moment = require("moment");
+
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
 
 class homeController {
   formateProduct = (products) => {
@@ -223,6 +228,125 @@ class homeController {
       // console.log(product);
     } catch (error) {
       console.error("💥 Error in homeController: get_product_details:", error);
+      return responseReturn(res, 500, {
+        error: error.message || "Something went wrong",
+      });
+    }
+  }
+
+  async submit_review(req, res) {
+    // console.log(req.body);
+
+    const { productId, rating, review, name } = req.body;
+
+    try {
+      await reviewModel.create({
+        productId,
+        name,
+        rating,
+        review,
+        date: moment(Date.now()).format("LL"),
+      });
+
+      let rat = 0;
+      const reviews = await reviewModel.find({
+        productId,
+      });
+
+      for (let i = 0; i < reviews.length; i++) {
+        rat = rat + reviews[i].rating;
+      }
+
+      let productRating = 0;
+      if (reviews.length !== 0) {
+        productRating = (rat / reviews.length).toFixed(1);
+      }
+
+      await productModel.findByIdAndUpdate(productId, {
+        rating: productRating,
+      });
+
+      return responseReturn(res, 201, {
+        message: "Review added successfully",
+      });
+    } catch (error) {
+      console.error("💥 Error in homeController: submit_review:", error);
+      return responseReturn(res, 500, {
+        error: error.message || "Something went wrong",
+      });
+    }
+  }
+
+  async get_reviews(req, res) {
+    // console.log(req.params);
+    const { productId } = req.params;
+    let { pageNo } = req.query;
+    // console.log(productId, pageNo);
+
+    pageNo = parseInt(pageNo);
+    const limit = parseInt(process.env.REVIEWS_PER_PAGE, 10) || 5; // 5
+    const skipPage = limit * (pageNo - 1);
+
+    try {
+      let getRating = await reviewModel.aggregate([
+        {
+          $match: {
+            productId: {
+              $eq: new ObjectId(productId),
+            },
+            rating: {
+              $not: {
+                $size: 0,
+              },
+            },
+          },
+        },
+        {
+          $unwind: "$rating",
+        },
+        {
+          $group: {
+            _id: "$rating",
+            count: {
+              $sum: 1,
+            },
+          },
+        },
+      ]);
+
+      let rating_review = [
+        { rating: 5, sum: 0 },
+        { rating: 4, sum: 0 },
+        { rating: 3, sum: 0 },
+        { rating: 2, sum: 0 },
+        { rating: 1, sum: 0 },
+      ];
+
+      for (let i = 0; i < rating_review.length; i++) {
+        for (let j = 0; j < getRating.length; j++) {
+          if (rating_review[i].rating === getRating[j]._id) {
+            rating_review[i].sum = getRating[j].count;
+            break;
+          }
+        }
+      }
+
+      const getAll = await reviewModel.find({ productId });
+      const reviews = await reviewModel
+        .find({
+          productId,
+        })
+        .skip(skipPage)
+        .limit(limit)
+        .sort({ createdAt: -1 });
+
+      return responseReturn(res, 200, {
+        reviews,
+        totalReview: getAll.length,
+        rating_review,
+      });
+    } catch (error) {
+      console.error("💥 Error in homeController: get_reviews:", error);
       return responseReturn(res, 500, {
         error: error.message || "Something went wrong",
       });
--- routes/home/homeRoutes.js
+++ routes/home/homeRoutes.js
@@ -7,5 +7,7 @@ router.get("/get-products", homeController.get_products);
 router.get("/price-range-latest-product", homeController.price_range_product);
 router.get("/query-products", homeController.query_products);
 router.get("/product-details/:slug", homeController.get_product_details);
+router.post("/customer/submit-review", homeController.submit_review);
+router.get("/customer/get-reviews/:productId", homeController.get_reviews);
 
 module.exports = router;
